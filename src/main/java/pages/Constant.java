8cc6d7356c04fb0f9beb195ef99fa30b11ef4133
nitesh <nitesh.barot31@gmail.com>
2023-07-05 15:25:50 +0530

    New Urls

--- src/main/java/pages/Constant.java
+++ src/main/java/pages/Constant.java
@@ -8,9 +8,9 @@ public class Constant {
     public static String url;
     static {
         if(ENVIRONMENT.equals("UTS-UK2QA")){
-            url="http://52.32.106.250:7779/";
+            url="https://stage.petcovergroup.com:7779/";
         } else if (ENVIRONMENT.equals("UTS-UK2Stage")) {
-            url="http://52.32.106.250:8889/";
+            url="https://stage.petcovergroup.com:8889/";
         }
         else if (ENVIRONMENT.equals("UTS-UK2production")) {
             url="https://fitlocal2.britishpetinsurance.co.uk:94/";
